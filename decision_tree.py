df669f9b021c61e5ea552506eadcb8ad586125f8
Victor Wang <wang.sw@hotmail.com>
2021-02-20 15:27:22 -0800

    hw1

---
+++ decision_tree.py
@@ -0,0 +1,54 @@
+# -------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4200- Assignment #1
+# TIME SPENT: how long it took you to complete the assignment
+# -----------------------------------------------------------*/
+
+# IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+
+# importing some Python libraries
+from sklearn import tree
+import matplotlib.pyplot as plt
+import csv
+
+db = []
+X = []
+Y = []
+
+# reading the data in a csv file
+with open('contact_lens.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0:  # skipping the header
+            db.append(row)
+            print(row)
+
+# transform the original training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
+# --> add your Python code here
+# X =
+dict = {"Young": 1, "Presbyopic": 2, "Prepresbyopic": 3,
+        "Myope": 1, "Hypermetrope": 2,
+        "No": 1, "Yes": 2,
+        "Reduced": 1, "Normal": 2}
+for row in db:
+    temp = []
+    for i in range(len(row)-1):
+        temp.append(dict.get(row[i]))
+    X.append(temp)
+
+# transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
+# --> addd your Python code here
+dict2 = {"No": 1, "Yes": 2}
+for row in db:
+    Y.append(dict2.get(row[len(row)-1]))
+
+# fitting the decision tree to the data
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(X, Y)
+
+# plotting the decision tree
+tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes', 'No'], filled=True,
+               rounded=True)
+plt.show()
---
+++ find_s.py
@@ -0,0 +1,49 @@
+#-------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4200- Assignment #1
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+
+#importing some Python libraries
+import csv
+
+num_attributes = 4
+db = []
+print("\n The Given Training Data Set \n")
+
+#reading the data in a csv file
+with open('contact_lens.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         db.append (row)
+         print(row)
+
+print("\n The initial value of hypothesis: ")
+hypothesis = ['0'] * num_attributes #representing the most specific possible hypothesis
+print(hypothesis)
+
+counter = 0
+
+
+#find the first positive training data in db and assign it to the vector hypothesis
+for row in db:
+    counter += 1
+    if row[4] == 'Yes':
+        hypothesis = row
+        break
+
+#find the maximally specific hypothesis according to your training data in db and assign it to the vector hypothesis (special characters allowed: "0" and "?")
+##--> add your Python code here
+for i in range(counter, len(db)):
+    if db[i][4] == 'Yes':
+        for j in range(len(hypothesis)-1):
+            if db[i][j] != hypothesis[j]:
+                hypothesis[j] = '?'
+
+print("\n The Maximally Specific Hypothesis for the given training examples found by Find-S algorithm:\n")
+print(hypothesis)
